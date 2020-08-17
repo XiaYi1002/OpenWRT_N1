2dc8b86718718592bd506267a2c27083b68541e8
XiaYi1002 <411194236@qq.com>
2020-08-17 16:18:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd openwrt
-rm -rf package/lean/luci-theme-argon  #删除源码自带的argon主题，因为最下面一个链接是增加了其他作者制作的argon主题
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
@@ -19,7 +17,8 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowS
 sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-package.git;master' feeds.conf.default
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon  #argon-主题
 git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
 git clone -b master https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome  #adguardhome广告过滤
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-theme-atmaterial package/luci-theme-atmaterial #atmaterial主题
+
+
