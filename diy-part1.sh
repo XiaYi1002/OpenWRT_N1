8326b492a7eded7b2e7a1d145a71346e9ca6011e
XiaYi1002 <411194236@qq.com>
2020-08-26 17:36:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,14 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowS
 # Add a feed source
 sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-package.git;master' feeds.conf.default
 
+
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
+#清理插件
+#rm -rf package/adguardhome
+#rm -rf package/luci-app-adguardhome
+#rm -rf package/luci-theme-atmaterial
+#rm -rf package/luci-app-openclash
+#添加
 git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
 svn co https://github.com/Lienol/openwrt/trunk/package/diy/adguardhome package/adguardhome  #adguardhome广告过滤
 svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome package/luci-app-adguardhome  #adguardhome广告过滤
